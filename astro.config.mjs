572521c59e7eaec5daf5d8e5e43442386de34564
Felix Dahle <felix.dahle@googlemail.com>
2026-04-24 11:35:12 +0200

    finished blog post + blog thumbnails

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,17 @@ import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fdahle.de',
   integrations: [mdx(), sitemap()],
-
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
   fonts: [
       {
           provider: fontProviders.local(),
